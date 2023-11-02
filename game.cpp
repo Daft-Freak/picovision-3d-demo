b004759dc99f4bbbdb26bbf27b560d3d683eb203
Charlie Birks <charlie@daft.games>
2023-11-02 11:53:59 +0000

    Convert to a cube model
    Also clean up all the old shaders

--- game.cpp
+++ game.cpp
@@ -6,6 +6,8 @@
 #include "32blit.hpp"
 #include "engine/api_private.hpp"
 
+#include "assets.hpp"
+#include "model.hpp"
 #include "render-3d.hpp"
 #include "vec4.hpp"
 
@@ -56,88 +58,9 @@ Mat4 frustum(float left, float right, float bottom, float top, float nearVal, fl
     return mat;
 }
 
-// "shaders"
-// in is xyz
-// out is xyzw rgb
-void noColShader(const uint8_t *in, Render3D::VertexOutData *out, const Render3D &r)
-{
-    auto fin = reinterpret_cast<const float *>(in);
-    FixedVec4 tmp(fin[0], fin[1], fin[2], 1.0f);
-    
-    // transform
-    tmp = r.get_model_view_projection() * tmp;
-
-    // pos
-    out->x = tmp.x;
-    out->y = tmp.y;
-    out->z = tmp.z;
-    out->w = tmp.w;
-
-    // col
-    out->r = 255;
-    out->g = 255;
-    out->b = 255;
-}
-
-// in is xyz rgb
-// out is xyzw rgb
-void colPassthroughShader(const uint8_t *in, Render3D::VertexOutData *out, const Render3D &r)
-{
-    auto fin = reinterpret_cast<const float *>(in);
-    FixedVec4 tmp(fin[0], fin[1], fin[2], 1.0f);
-    
-    // transform
-    tmp = r.get_model_view_projection() * tmp;
-
-    // pos
-    out->x = tmp.x;
-    out->y = tmp.y;
-    out->z = tmp.z;
-    out->w = tmp.w;
-
-    // col
-    out->r = fin[3] * 255.0f;
-    out->g = fin[4] * 255.0f;
-    out->b = fin[5] * 255.0f;
-}
-
-// in is xyz nx ny nx
-// out is xyzw rgb
-void litShader(const uint8_t *in, Render3D::VertexOutData *out, const Render3D &r)
-{
-    auto fin = reinterpret_cast<const float *>(in);
-    FixedVec4 tmp(fin[0], fin[1], fin[2], 1.0f);
-    
-    // transform
-    tmp = r.get_model_view_projection() * tmp;
-
-    // pos
-    out->x = tmp.x;
-    out->y = tmp.y;
-    out->z = tmp.z;
-    out->w = tmp.w;
-
-    // col
-    Vec3 light(-0.577350269f, -0.577350269f, -0.577350269f);
-
-    Vec4 nor(fin[3], fin[4], fin[5], 1.0f);
-    nor = r.get_model_view() * nor;
-    float len = sqrt(nor.x * nor.x + nor.y * nor.y + nor.z * nor.z);
-    nor.x /= len;
-    nor.y /= len;
-    nor.z /= len;
-
-    float dot = light.x * nor.x + light.y * nor.y + light.z * nor.z;
-
-    out->r = std::max(dot, 0.0f) * 255.0f;
-    out->g = std::max(dot, 0.0f) * 255.0f;
-    out->b = std::max(dot, 0.0f) * 255.0f;
-}
-
-//
-
-Render3D r3d;
-float ang = 0.0f, ang2 = 0.0f;
+static Render3D r3d;
+static float ang = 0.0f, ang2 = 0.0f;
+static Model model(asset_cube);
 
 void init()
 {
@@ -159,54 +82,6 @@ void update(uint32_t time)
 
 void render(uint32_t time)
 {
-
-    // cube with normals
-    const float vertices_with_normals[]
-    {
-        -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, //0
-        -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, //2
-         1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, //3
-         1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, //3
-         1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, //1
-        -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, //0
-
-        -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, //4
-         1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, //5
-         1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, //7
-         1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, //7
-        -1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, //6
-        -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, //4
-
-        -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, //0
-         1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, //1
-         1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, //5
-         1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, //5
-        -1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, //4
-        -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, //0
-
-         1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, //1
-         1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f, //3
-         1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, //7
-         1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, //7
-         1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f, //5
-         1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, //1
-
-         1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, //3
-        -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, //2
-        -1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, //6
-        -1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, //6
-         1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, //7
-         1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, //3
-
-        -1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f, //2
-        -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, //0
-        -1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, //4
-        -1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, //4
-        -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, //6
-        -1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f, //2
-
-    };
-
     /*Mat4 m;
     m.identity();
     m.rotate(ang, Vec3{0.0f, 1.0f, 0.0f});
@@ -214,12 +89,9 @@ void render(uint32_t time)
     r3d.set_model_view(m);*/
     r3d.set_model_view(Mat4::translation(Vec3(0.0f, 0.0f, -4.0f)) * Mat4::rotation(ang, Vec3{0.0f, 1.0f, 0.0f}) * Mat4::rotation(ang2, Vec3{1.0f, 0.0f, 0.0f}));
 
-    r3d.set_vertex_stride(6 * sizeof(float));
-
     auto vert_start = now_us();
-
-    r3d.set_vertex_shader(litShader);
-    r3d.draw(6 * 6, reinterpret_cast<const uint8_t *>(vertices_with_normals));
+    r3d.set_vertex_shader(model_lit_shader);
+    model.draw(r3d);
     auto vert_end = now_us();
 
     auto frag_start = now_us();
