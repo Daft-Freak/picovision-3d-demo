a91af929093a8547a4f71d1f307952b61e0625f3
Charlie Birks <charlie@daft.games>
2023-11-06 16:03:25 +0000

    Add a little zoom intro

--- game.cpp
+++ game.cpp
@@ -61,8 +61,13 @@ Mat4 frustum(float left, float right, float bottom, float top, float nearVal, fl
 
 static Render3D r3d;
 static float ang = 0.0f, ang2 = 0.0f;
+static float scale = 0.1f;
+
 static Model model(asset_picovision);
 static Surface *picovision_tex;
+
+static uint32_t start_time;
+static bool done_zoom = false;
 
 void init()
 {
@@ -78,12 +83,24 @@ void init()
 
     picovision_tex = Surface::load(asset_picovision_tex);
     r3d.set_texture(picovision_tex, 0);
+
+    start_time = now();
 }
 
 void update(uint32_t time)
 {
     ang += 0.25f;
     ang2 -= 0.15f;
+
+    // zoom in after a delay
+    if(!done_zoom && time - start_time > 4000 && scale < 45.0f)
+        scale *= 1.05f;
+    else if(scale > 40.0f)
+    {
+        // then back down a little
+        done_zoom = true;
+        scale = std::max(scale - 0.2f, 40.0f);
+    }
 }
 
 void render(uint32_t time)
@@ -91,10 +108,10 @@ void render(uint32_t time)
     auto render_start = now_us();
 
     auto translation = FixedMat4<>::translation(Vec3(0.0f, 0.0f, -4.0f));
-    auto scale = FixedMat4<>::scale({40.0f, 40.0f, 40.0f});
+    auto scale_mat = FixedMat4<>::scale({scale, scale, scale});
     auto rot_y = FixedMat4<>::rotation(ang, Vec3{0.0f, 1.0f, 0.0f});
     auto rot_x = FixedMat4<>::rotation(ang2, Vec3{1.0f, 0.0f, 0.0f});
-    r3d.set_model_view(translation * scale * rot_y * rot_x);
+    r3d.set_model_view(translation * scale_mat * rot_y * rot_x);
 
     auto vert_start = now_us();
     r3d.set_position_shader(fixed32_mvp_pos_shader);
